4e0acce4382080b5426cce09f8834bee6d9b3a1a
Yola Kristina Cendani <71878813+YolaKC@users.noreply.github.com>
2021-05-15 02:30:54 +0700

    Update aksilogin.php

--- aksilogin.php
+++ aksilogin.php
@@ -6,13 +6,12 @@ $password="";
 $db="quis 2";
 $koneksi=mysqli_connect($server,$username,$password,$db);
 
+
 $user=$_POST['username'];
 $pass=$_POST['password'];
 
 $sql=mysqli_query($koneksi, "Select * from akun where username = '$user' and password = '$pass'");
-
 $cek=mysqli_num_rows($sql);
-
 
 ?>
 <!DOCTYPE html>
@@ -22,18 +21,16 @@ $cek=mysqli_num_rows($sql);
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <tr>
-        <?php
-        if ($cek >0){
-            session_start();
-            $_SESSION['username']=$user;
-            $_SESSION['password']=$password;
-            echo "<br><br><br><br><center>Selamat Datang <b>$user</b>, Anda Berhasil Login<br>";
-        } else {
-            echo "<br><br><br><br><center>Username dan Password Anda Salah<br>";
-            echo "Silahkan <a href=login.php>Login</a> Kembali</center>";
-        }
-        ?>
-    </tr>
+    <?php
+    if ($cek >0){
+        session_start();
+        $_SESSION['username']=$user;
+        $_SESSION['password']=$password;
+        echo "<br><br><br><br><center>Selamat Datang <b>$user</b>, Anda Berhasil Login<br>";
+    } else {
+        echo "<br><br><br><br><center>Username dan Password Anda Salah<br>";
+        echo "Silahkan <a href=login.php>Login</a> Kembali</center>";
+    }
+    ?>
 </body>
 </html>
